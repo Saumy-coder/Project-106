702a6a859cd05df17abdc60ec3b0ae93754ebd45
Saumy-coder <81005589+Saumy-coder@users.noreply.github.com>
2022-12-22 02:52:06 +0530

    Add files via upload

---
+++ Walkers.py
@@ -0,0 +1,17 @@
+import cv2
+body_classifier = cv2.CascadeClassifier('haarcascade_fullbody.xml')
+
+cap = cv2.VideoCapture('walking.avi')
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    bodies = body_classifier.detectMultiScale(gray,1.2,3)
+    for (x,y,w,h) in bodies:
+       cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3)
+    cv2.imshow('vid',frame)
+    if cv2.waitKey(1) == 32:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
